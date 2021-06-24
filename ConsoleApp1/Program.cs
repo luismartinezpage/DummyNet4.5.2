0ac936eb7da244cc160bb16f15d458a8fa57f837
root <lmartinez@viewnext.com>
2021-06-24 13:17:49 +0200

    Adding app net

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa de prueba con framework 4.5.2");
+            Console.WriteLine("Pulsa cualquier tecla");
+            Console.ReadKey();                
+        }
+    }
+}
